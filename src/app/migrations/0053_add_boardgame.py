dd82c4e8a3dcc2daf0da84130d165a601dd6a9f3
dmintz7 <54006804+dmintz7@users.noreply.github.com>
2026-01-30 17:21:53 -0500

    Fix migrations Update docker-compose.yml

--- src/app/migrations/0053_add_boardgame.py
+++ src/app/migrations/0053_add_boardgame.py
@@ -38,6 +38,8 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.DeleteModel(name='BoardGame',),
+        migrations.DeleteModel(name='HistoricalBoardGame',),
         migrations.CreateModel(
             name='BoardGame',
             fields=[
