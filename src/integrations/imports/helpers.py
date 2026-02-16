2f127c7b1c6ca96d1bc077653db9033a10c3bf00
dmintz7 <54006804+dmintz7@users.noreply.github.com>
2026-02-16 17:04:09 -0500

    Add External Id Check

--- src/integrations/imports/helpers.py
+++ src/integrations/imports/helpers.py
@@ -745,26 +745,31 @@ def get_or_create_item(
     season_number=None,
     episode_number=None,
 ):
-    item_kwargs = {
-        "media_id": source_id,
-        "source": source_key,
-        "media_type": media_type,
-    }
+    source_enum = MetadataSources(source_key)
+    ext_qs = ExternalID.objects.select_related("item").filter(metadata_source=source_enum, metadata_source_identifier=str(source_id), item__media_type=media_type,)
+    if ext_qs:
+        item = ext_qs.first().item
+    else:
+        item_kwargs = {
+            "media_id": source_id,
+            "source": source_key,
+            "media_type": media_type,
+        }
 
-    if season_number is not None:
-        item_kwargs["season_number"] = season_number
+        if season_number is not None:
+            item_kwargs["season_number"] = season_number
 
-    if episode_number is not None:
-        item_kwargs["episode_number"] = episode_number
+        if episode_number is not None:
+            item_kwargs["episode_number"] = episode_number
 
-    defaults = {
-        "title": metadata["title"],
-        "image": metadata["image"],
-    }
+        defaults = {
+            "title": metadata["title"],
+            "image": metadata["image"],
+        }
 
-    item, _ = app.models.Item.objects.get_or_create(
-        **item_kwargs,
-        defaults=defaults,
-    )
+        item, _ = app.models.Item.objects.get_or_create(
+            **item_kwargs,
+            defaults=defaults,
+        )
 
     return item
