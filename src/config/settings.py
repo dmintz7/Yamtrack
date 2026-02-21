8275099bce6a3cdb5421cf989cd5cbe2dfd261fd
dmintz7 <54006804+dmintz7@users.noreply.github.com>
2026-02-20 22:46:37 -0500

    Change for my fork

--- src/config/settings.py
+++ src/config/settings.py
@@ -885,4 +885,26 @@ LOGIN_REQUIRED_EXEMPT = [
     r"^/list/\d+/json/?$",  # Public list JSON exports
 ]
 
+
+import logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '%(asctime)s  -  %(levelname)-8s  -  %(module)s:%(funcName)s:%(lineno)d  -  %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': logging.DEBUG if DEBUG else logging.INFO,
+    },
+}
+
 TRAKT_BULK_PAGE_SIZE = config("TRAKT_BULK_PAGE_SIZE", default="1000")
