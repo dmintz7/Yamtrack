3cfa31c592f9f215b084f14d66d09a487be2002c
dmintz7 <54006804+dmintz7@users.noreply.github.com>
2026-02-20 22:14:26 -0500

    Add TVDB (Skyhook) as Source

--- src/integrations/imports/trakt.py
+++ src/integrations/imports/trakt.py
@@ -320,13 +320,13 @@ class TraktImporter:
                 source_key,
                 **kwargs,
             )
+        except KeyError as e:
+            if 'season/' in e.args[0]:
+                logger.debug(f"Ignoring unknown season {e.args[0]} for {source_key} {source_id} {title}")
+                return None
+            raise
         except services.ProviderAPIError as error:
             if error.status_code == requests.codes.not_found:
-                if media_type == MediaTypes.SEASON.value:
-                    title = f"{title} S{season_number}"
-                self.warnings.append(
-                    f"{title}: not found in {Sources.TMDB.label} with ID {tmdb_id}.",
-                )
                 return None
             raise
 
